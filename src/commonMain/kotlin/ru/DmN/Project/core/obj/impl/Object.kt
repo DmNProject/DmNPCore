f8f0022acd46653b25f6f99bb46f22e1bbd10a9a
Domaman202 <vip.domaman@mail.ru>
2021-05-06 19:38:48 +0700

    .

---
+++ src/commonMain/kotlin/ru/DmN/Project/core/obj/impl/Object.kt
@@ -0,0 +1,6 @@
+package ru.DmN.Project.core.obj.impl
+
+import ru.DmN.Project.core.obj.api.v0.IObject
+import ru.DmN.Project.core.obj.api.v0.ObjType
+
+class Object(override val name: String, override val type: ObjType) : IObject
