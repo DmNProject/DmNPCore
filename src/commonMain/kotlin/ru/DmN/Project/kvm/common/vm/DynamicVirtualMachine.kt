23f3a79d06c12893231d8e1db9dca8924f1986ab
Domaman202 <vip.domaman@mail.ru>
2021-05-30 12:08:55 +0700

    Create DynamicVirtualMachine.kt

---
+++ src/commonMain/kotlin/ru/DmN/Project/kvm/common/vm/DynamicVirtualMachine.kt
@@ -0,0 +1,13 @@
+package ru.DmN.Project.kvm.common.vm
+
+import ru.DmN.Project.core.vm.v0.IVirtualMachine
+
+class DynamicVirtualMachine : IVirtualMachine<ByteArray> {
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun eval(code: ByteArray) {
+        TODO("Not yet implemented")
+    }
+}
