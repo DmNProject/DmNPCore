e27f3264c40522697dd20dd68439d18d6c317749
Domaman202 <vip.domaman@mail.ru>
2021-06-01 13:25:08 +0700

    .

--- src/commonMain/kotlin/ru/DmN/Project/core/util/Utils.kt
+++ src/commonMain/kotlin/ru/DmN/Project/core/util/Utils.kt
@@ -4,21 +4,28 @@ import ru.DmN.Project.core.obj.IEO
 import ru.DmN.Project.core.obj.IObject
 import ru.DmN.Project.kvm.common.utils.AtomicInt
 
-fun objectEquals(o1: IEO<*>, o2: IObject, i: AtomicInt = AtomicInt()): Boolean {
-    i.inc()
-    if (o1.name == o2.name)
-        return true
-    //
-    i.inc()
-    for (e in o1.extends)
-        if (e.name == o2.name)
+object Utils {
+    fun objectEquals(o1: IEO<*>, o2: IObject): Boolean {
+        if (o1.name == o2.name)
             return true
-    //
-    i.inc()
-    for (e in o1.extends)
-        if (e is IEO<*> && objectEquals(e, o2))
+        for (e in o1.extends)
+            if (e.name == o2.name || (e is IEO<*> && objectEquals(e, o2)))
+                return true
+        return false
+    }
+
+    fun objectEquals(o1: IEO<*>, o2: IObject, i: AtomicInt): Boolean {
+        if (o1.name == o2.name)
             return true
-    //
-    i.i -= 3
-    return false
+        i.inc()
+        for (e in o1.extends)
+            if (e.name == o2.name)
+                return true
+        i.inc()
+        for (e in o1.extends)
+            if (e is IEO<*> && objectEquals(e, o2))
+                return true
+        i.i -= 3
+        return false
+    }
 }
--- src/commonMain/kotlin/ru/DmN/Project/kvm/common/obj/api/IFunction.kt
+++ src/commonMain/kotlin/ru/DmN/Project/kvm/common/obj/api/IFunction.kt
@@ -2,7 +2,7 @@ package ru.DmN.Project.kvm.common.obj.api
 
 import ru.DmN.Project.core.obj.IEO
 import ru.DmN.Project.core.obj.IObject
-import ru.DmN.Project.core.util.objectEquals
+import ru.DmN.Project.core.util.Utils.objectEquals
 import ru.DmN.Project.kvm.common.utils.AtomicInt
 import ru.DmN.Project.kvm.common.vm.Call
 
@@ -12,7 +12,27 @@ interface IFunction : IObject {
 
     fun call(call: Call) = code?.let { call.vm.eval(it) }
 
-    fun argEquals(args1: Iterator<IObject>, counter: AtomicInt = AtomicInt()): Boolean {
+    fun argEquals(args1: Iterator<IObject>): Boolean {
+        var i = 0
+        var j = 0
+
+        val iterator = args.iterator()
+        while (iterator.hasNext()) {
+            if (!args1.hasNext())
+                return false
+            i++
+            //
+            val o1 = iterator.next()
+            val o2 = args1.next()
+            //
+            if (o2 is IEO<*> && objectEquals(o2, o1))
+                j++
+        }
+
+        return i == j
+    }
+
+    fun argEquals(args1: Iterator<IObject>, counter: AtomicInt): Boolean {
         var i = 0
         var j = 0
 
