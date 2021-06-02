f3a7c8cc804dab905bd75c84101e1366154d3e1e
Domaman202 <vip.domaman@mail.ru>
2021-06-02 14:05:36 +0700

    ,

--- src/commonMain/kotlin/ru/DmN/Project/kvm/common/obj/api/KObject.kt
+++ src/commonMain/kotlin/ru/DmN/Project/kvm/common/obj/api/KObject.kt
@@ -2,16 +2,19 @@ package ru.DmN.Project.kvm.common.obj.api
 
 import ru.DmN.Project.core.data.api.IDS
 import ru.DmN.Project.core.data.api.IES
+import ru.DmN.Project.core.data.impl.IDSImpl
+import ru.DmN.Project.core.data.impl.IESImpl
 import ru.DmN.Project.core.obj.IDO
 import ru.DmN.Project.core.obj.IEO
 import ru.DmN.Project.core.obj.IObject
 import ru.DmN.Project.core.obj.ObjType
 import ru.DmN.Project.kvm.common.data.api.IFS
+import ru.DmN.Project.kvm.common.data.impl.IFSImpl
 
 open class KObject (
     override inline val name: String,
     override inline val type: ObjType,
-    override val defines: IDS<IObject>,
-    override val functions: IFS,
-    override val extends: IES<IObject>
+    override val defines: IDS<IObject> = IDSImpl(),
+    override val functions: IFS = IFSImpl(),
+    override val extends: IES<IObject> = IESImpl()
 ) : IEO<IObject>, IDO<IObject>, IFunctionsContainer
--- src/commonTest/kotlin/ru/DmN/Project/test/UnnamedTests.kt
+++ src/commonTest/kotlin/ru/DmN/Project/test/UnnamedTests.kt
@@ -1,15 +1,12 @@
 package ru.DmN.Project.test
 
 import ru.DmN.Project.kvm.common.obj.KawaiiType
+import ru.DmN.Project.kvm.common.obj.api.KObject
 import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotEquals
 
 class UnnamedTests {
     @Test
     fun test() {
-        assertEquals(KawaiiType.OBJ, KawaiiType.OBJ)
-        assertNotEquals(KawaiiType.OBJ, KawaiiType.VAR)
-        assertNotEquals(KawaiiType.VAR, KawaiiType())
+        println(KObject("test", KawaiiType.VAR).name == KObject("test", KawaiiType.VAR).name)
     }
 }
