30cfee7e5cfe202889d3c25a68907962ca4354d1
Domaman202 <vip.domaman@mail.ru>
2021-06-03 08:24:39 +0700

    Update SpeedTest.kt

--- src/jvmTest/kotlin/ru/DmN/Project/kvm/test/SpeedTest.kt
+++ src/jvmTest/kotlin/ru/DmN/Project/kvm/test/SpeedTest.kt
@@ -8,6 +8,12 @@ class SpeedTest {
     @Test
     fun test() {
         //
+        val code = intArrayOf(
+            1, 1,
+            3, 1, 0, 13, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33,
+            9, 0, 7, 112, 114, 105, 110, 116, 108, 110
+        )
+        //
         val vm = DynamicVirtualMachine()
         //
         vm.init()
@@ -15,11 +21,9 @@ class SpeedTest {
         val sw = StopWatch()
         sw.start()
         //
-        vm.eval(intArrayOf(
-            1, 1,
-            3, 1, 0, 13, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33,
-            9, 0, 7, 112, 114, 105, 110, 116, 108, 110
-        ))
+        for (i in 0..999) {
+            vm.eval(code)
+        }
         //
         sw.stop()
         println(sw)
