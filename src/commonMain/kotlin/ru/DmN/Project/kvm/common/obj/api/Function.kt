770f4cb34ac90f4857118d5f739cfd9bcad066c3
Domaman202 <vip.domaman@mail.ru>
2021-06-03 09:08:59 +0700

    Update Function.kt

--- src/commonMain/kotlin/ru/DmN/Project/kvm/common/obj/api/Function.kt
+++ src/commonMain/kotlin/ru/DmN/Project/kvm/common/obj/api/Function.kt
@@ -7,10 +7,5 @@ import ru.DmN.Project.kvm.common.obj.KawaiiType
 abstract class Function : IFunction {
     override val type: ObjType = KawaiiType.VM
     override val code: IntArray? = null
-    override val args: Iterable<IObject> = object : Iterable<IObject> {
-        override fun iterator(): Iterator<IObject> = object : Iterator<IObject> {
-            override fun hasNext(): Boolean = false
-            override fun next(): IObject = throw Exception()
-        }
-    }
+    override val args: List<IObject> = emptyList()
 }
