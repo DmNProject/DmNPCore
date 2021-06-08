689fe067f526e1b967ca658df338a55f5edf7dcb
Domaman202 <vip.domaman@mail.ru>
2021-06-08 13:56:17 +0700

    Update Utils.kt

--- src/commonMain/kotlin/ru/DmN/Project/core/util/Utils.kt
+++ src/commonMain/kotlin/ru/DmN/Project/core/util/Utils.kt
@@ -5,20 +5,20 @@ import ru.DmN.Project.core.obj.IObject
 
 object Utils {
     fun objectEquals(o1: IEO<*>, o2: IObject): Boolean {
-        if (o1.name == o2.name)
+        if (o1 == o2)
             return true
         for (e in o1.extends)
-            if (e.name == o2.name || (e is IEO<*> && objectEquals(e, o2)))
+            if (e == o2 || (e is IEO<*> && objectEquals(e, o2)))
                 return true
         return false
     }
 
     fun objectEquals(o1: IEO<*>, o2: IObject, i: AtomicInt): Boolean {
-        if (o1.name == o2.name)
+        if (o1 == o2)
             return true
         i.value++
         for (e in o1.extends)
-            if (e.name == o2.name)
+            if (e == o2)
                 return true
         i.value++
         for (e in o1.extends)
